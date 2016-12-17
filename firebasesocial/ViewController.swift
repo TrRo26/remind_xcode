a8462561cbb8ad3ee42f09d4ad59a57f4da4d1a0
andrew van beek <andrewvanbeek90@gmail.com>
2016-12-17 09:16:02 -0600

    adds print messages to logining in and out, as well as the unique facebook user identifier-totally safe,

--- firebasesocial/ViewController.swift
+++ firebasesocial/ViewController.swift
@@ -9,20 +9,33 @@
 import UIKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
         loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print("Yakakaaaa")
+            return
+        }
+        
+        print("Successfully logged in with facebook...")
+        print(FBSDKAccessToken.current().userID)
+    }
+    
 }
-
+ 
