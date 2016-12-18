df63809d0428adcad72810deb6013be6e2648e8a
andrew van beek <andrewvanbeek90@gmail.com>
2016-12-18 15:57:18 -0600

    gets input to work

--- firebasesocial/ItemPostViewController.swift
+++ firebasesocial/ItemPostViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ItemPostViewController: UIViewController {
     var name = ""
@@ -16,6 +17,32 @@ class ItemPostViewController: UIViewController {
     
     @IBAction func buttonClick(_ sender: UIButton) {
         print(textFieldName.text!)
+        name = textFieldName.text!
+        let gotoplace: String = "http://remind-dbc.herokuapp.com/items"
+        let newList: [String: Any] = ["name": name, "list_id" : 1]
+        Alamofire.request(gotoplace, method: .post, parameters: ["item": newList], encoding: JSONEncoding.default)
+            .responseJSON { response in
+                
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error calling POST on /lists")
+                    print(response.result.error!)
+                    return
+                }
+                // make sure we got some JSON since that's what we expect
+                guard let json = response.result.value as? [String: Any] else {
+                    print("didn't get list object as JSON from API")
+                    print("Error: \(response.result.error)")
+                    return
+                }
+                guard let listName = json["name"] as? String else {
+                    print("Could not get list name from JSON")
+                    return
+                }
+                print("The title is: " + listName)
+                
+        }
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
