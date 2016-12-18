7e8ec97de615f2c9364a8d659e926dc99ccecc92
andrew van beek <andrewvanbeek90@gmail.com>
2016-12-17 21:48:41 -0600

    adds input

---
+++ firebasesocial/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  firebasesocial
+//
+//  Created by Apprentice on 12/17/16.
+//  Copyright © 2016 Apprentice. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: NSObject {
+
+}
