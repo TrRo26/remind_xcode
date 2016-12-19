aa0bccb98f5f3699f96e77fdd2f8978e0e441ea8
Travis Roy <email": null>
2016-12-19 16:36:40 -0600

    adds map

---
+++ firebasesocial/map.swift
@@ -0,0 +1,56 @@
+//
+//  map.swift
+//  firebasesocial
+//
+//  Created by Apprentice on 12/19/16.
+//  Copyright © 2016 Apprentice. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class map: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
