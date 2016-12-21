1b213519f4fb0765f44b2e827effe8b9a13db122
Travis Roy <email": null>
2016-12-20 21:34:26 -0600

    Clean up

---
+++ firebasesocial/ListView.swift
@@ -0,0 +1,50 @@
+//
+//  ListView.swift
+//  firebasesocial
+//
+//  Created by Apprentice on 12/18/16.
+//  Copyright © 2016 Apprentice. All rights reserved.
+//
+
+import UIKit
+
+class ListView: UITableViewCell, UIViewController, UITableViewDelegate {
+    
+    var tableItems = ["a", "b", "c"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return (tableItems.count)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tableItems[indexPath.row]
+        
+        return cell
+    }
+
+}
